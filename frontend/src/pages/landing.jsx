f4ab4253a79cc33f4f4f0b8866ba9cbc9ac5b6f0
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-08-20 20:23:23 +0530

    landing page

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import '../App.css';
+import { Link } from 'react-router-dom';
 
 export default function LandingPage() {
     return (
-        <div> Landing Page </div>
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Meet-Up</h2>
+                </div>
+                <div className='navlist'>
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role='button'> <p> Login </p> </div>
+                </div>
+            </nav>
+
+            <div className='landingMainContainer'>
+                <div>
+                    <h1><span style={{color: "#FF9839"}}> Connect </span> with your <br/> Loved Ones</h1>
+                    <p>Cover Distance by Meet-Up</p>
+                    <div role='button'>
+                        <Link to={"/home"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+                    <img src='/mobile.png' alt='' />
+                </div>
+            </div>
+
+        </div>
     );
 }
