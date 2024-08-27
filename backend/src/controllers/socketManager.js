a3324f7e06ae774d5693032c17f52ab2b30f3390
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-08-27 11:13:54 +0530

    socket io

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,5 +1,6 @@
 import { Server } from "socket.io";
 
+
 let connections = {};
 let messages = {};
 let timeOnline = {};
@@ -13,10 +14,12 @@ export const connectToSocket = (server) => {
             credentials: true
         }
     });
+ 
+    io.on("connection", (socket) => {
 
-    io.on("connection", (req, res) => {
-
-        Socket.on("join-call", (path) => {
+        console.log("Something Connected"); 
+ 
+        socket.on("join-call", (path) => {
             
             if(connections[path] == undefined) {
                 connections[path] = [];
@@ -29,7 +32,7 @@ export const connectToSocket = (server) => {
                 io.to(connections[path][a]).emit("user-joined", socket.id, connections[path]);
             }
 
-            if(messages[path] != undefined) {
+            if(messages[path] !== undefined) {
                 for(let a=0; a < messages[path].length; ++a) {
                     io.to(socket.id).emit("chat-message", messages[path][a]['data'],
                         messages[path][a]['sender'], messages[path][a]['socket-id-sender'])
@@ -37,7 +40,7 @@ export const connectToSocket = (server) => {
             }
         })
 
-        Socket.on("signal", (toId, message) => {
+        socket.on("signal", (toId, message) => {
             io.to(toId).emit("signal", socket.id, message);
         })
 
@@ -45,7 +48,7 @@ export const connectToSocket = (server) => {
             
             const [matchingRoom, found] = Object.entries(connections)
               .reduse(([room, isFound], [roomKey, roomValue]) => {
-                 if(!isFound && roomValue.includes(socketKey)) {
+                 if(!isFound && roomValue.includes(socket.id)) {
                     return [roomKey, true];
                  }
 
@@ -53,8 +56,8 @@ export const connectToSocket = (server) => {
 
               }, ['', false] );
 
-            if(found == true) {
-                if(message[matchingRoom] == undefined) {
+            if(found === true) {
+                if(messages[matchingRoom] == undefined) {
                     messages[matchingRoom] = [];
                 }
 
