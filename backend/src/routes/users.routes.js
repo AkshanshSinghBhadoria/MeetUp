81d3a5834e8a55837fd010a1efe3913432068c03
Akshansh Singh Bhadoria <133026950+AkshanshSinghBhadoria@users.noreply.github.com>
2024-09-02 12:18:51 +0530

    final changes

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -6,6 +6,6 @@ const router = Router();
 router.route("/login").post(login);
 router.route("/register").post(register);
 router.route("/add_to_activity").post(addToHistory);
-router.route("/get_all_activity").post(getUserHistory);
+router.route("/get_all_activity").get(getUserHistory);
 
 export default router;
